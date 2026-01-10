db3f32acbf92a51ee518e83c322c28237999dfbc
Jehoi-ga-ada <sayajehoiada@gmail.com>
2026-01-10 16:18:20 +0700

    feat(cmd): implement main logic for TradeAdapter and logging

--- cmd/pulse/main.go
+++ cmd/pulse/main.go
@@ -1,9 +1,62 @@
 package main
 
 import (
-	"fmt"
+	"context"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	// "google.golang.org/protobuf/encoding/protojson"
+	marketv1 "github.com/Jehoi-ga-ada/aegis-genome/gen/go/market/v1"
+	"github.com/Jehoi-ga-ada/aegis-pulse/internal/adapters/binance"
+	"github.com/redis/go-redis/v9"
 )
 
 func main() {
-	fmt.Println("Aegis Pulse")
+	// 1. Setup cancellation
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer stop()
+
+	// 2. Setup Redis (Infrastructure)
+	rdb := redis.NewClient(&redis.Options{Addr: "localhost: 6739"})
+	defer rdb.Close()
+
+	// 3. Start Adapter
+	adapter := &binance.TradeAdapter{}
+	eventChan, err := adapter.Subscribe(ctx, "btcusdt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("Adapter started. Listening for trades...")
+
+	ticker := time.NewTicker(500 * time.Millisecond)
+	defer ticker.Stop()
+
+	var latestEvent *marketv1.MarketEvent
+
+	for {
+		select {
+		case <-ctx.Done():
+			log.Println("Shutting down...")
+			return
+		case event, ok := <-eventChan:
+			if !ok {
+				return
+			}
+			// 1. UPDATE: Always capture the newest data in memory.
+			// This is an instant operation that never blocks.
+			latestEvent = event
+
+		case <-ticker.C:
+			// 2. LOG: Only print the latest snapshot twice per second.
+			if latestEvent != nil {
+				price := latestEvent.GetTick().Price
+				latency := (latestEvent.ReceivedTimeNs - latestEvent.EventTimeNs) / 1_000_000
+				log.Printf("Price: %s | Latency: %dms", price, latency)
+			}
+		}
+	}
 }
--- go.mod
+++ go.mod
@@ -10,8 +10,11 @@ require (
 
 require (
 	github.com/andybalholm/brotli v1.2.0 // indirect
+	github.com/cespare/xxhash/v2 v2.3.0 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/gorilla/websocket v1.5.3 // indirect
+	github.com/redis/go-redis/v9 v9.17.2 // indirect
 	github.com/youmark/pkcs8 v0.0.0-20240726163527-a2c0da244d78 // indirect
 	golang.org/x/crypto v0.46.0 // indirect
 	golang.org/x/net v0.47.0 // indirect
